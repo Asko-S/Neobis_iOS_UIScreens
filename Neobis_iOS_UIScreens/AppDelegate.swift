9bb94cb52ce2d0ec93fbccae1795f88aa352bf7a
soronbekov-a-auca-2021 <soronbekov_a@auca.kg>
2023-08-23 04:29:34 +0600

    Final version

--- Neobis_iOS_UIScreens/AppDelegate.swift
+++ Neobis_iOS_UIScreens/AppDelegate.swift
@@ -1,3 +1,4 @@
+
 //  AppDelegate.swift
 //  Neobis_iOS_UIScreens
 //  Created by Askar Soronbekov
@@ -7,14 +8,15 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    
+    var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
         return true
     }
-    
-    // MARK: UISceneSession Lifecycle
     
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
@@ -28,4 +30,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
+
 
